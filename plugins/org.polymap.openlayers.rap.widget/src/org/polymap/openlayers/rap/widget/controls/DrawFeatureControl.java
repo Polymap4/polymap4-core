59563b2c7f8ccb15aec64d2e02fd8de48a4cc62c
fb71 <falko@polymap.de>
2013-07-18 17:58:27 +0200

    #219: Formular „bearbeiten“ öffnet sich nicht (http://polymap.org/atlas/ticket/219)   - patch possible race cond in Openlayers

--- plugins/org.polymap.openlayers.rap.widget/src/org/polymap/openlayers/rap/widget/controls/DrawFeatureControl.java
+++ plugins/org.polymap.openlayers.rap.widget/src/org/polymap/openlayers/rap/widget/controls/DrawFeatureControl.java
@@ -15,6 +15,7 @@
 package org.polymap.openlayers.rap.widget.controls;
 
 import org.polymap.openlayers.rap.widget.layers.VectorLayer;
+import org.polymap.openlayers.rap.widget.util.Stringer;
 
 /**
  * 
@@ -41,55 +42,66 @@ public class DrawFeatureControl
      * @param handler The handler to use to draw the feature. One of the
      *        <code>HANDLER_XXX</code> constants.
      */
-	public DrawFeatureControl(VectorLayer layer, String handler) {
-		super.create( "new OpenLayers.Control.DrawFeature(" + layer.getJSObjRef() + ", " + handler + ");" );
-		
-        addObjModCode( "OpenLayers.Util.extend(" + getJSObjRef() + ", {"
-                + "activate: function() {"
-                + "    OpenLayers.Control.DrawFeature.prototype.activate.apply(this, arguments);"
-                + "    if (!this.keyboardHandler) {"
-                + "        this.keyboardCallbacks = { keydown: this.handleKeyDown };"
-                + "        this.keyboardHandler = new OpenLayers.Handler.Keyboard(this, this.keyboardCallbacks, {});"
-                + "    }"
-                + "    this.keyboardHandler.activate();"
-                + "},"
-                + "deactivate: function() {"
-                + "    OpenLayers.Control.DrawFeature.prototype.deactivate.apply(this, arguments);"
-                + "    this.keyboardHandler.deactivate();"
-                + "},"
-                + "destroy: function() {"
-                + "    OpenLayers.Control.ModifyFeature.prototype.destroy.apply(this, arguments);"
-                + "    if (this.keyboardHandler) {"
-                + "        this.keyboardHandler.destroy();"
-                + "    }"
-                + "},"
-                + "handleKeyDown: function (evt) {"
-                + "    var handled = false;"
-                + "    switch (evt.keyCode) {"
-                + "    case 90:"  // z
-//                + "        if (evt.metaKey || evt.ctrlKey) {"
-                + "            this.undo();"
-                + "            handled = true;"
-//                + "        }"
-                + "        break;"
-                + "    case 89:"  // y
-//                + "        if (evt.metaKey || evt.ctrlKey) {"
-                + "            this.redo();"
-                + "            handled = true;"
-//                + "        }"
-                + "        break;"
-                + "    case 27:"  // esc
-                + "        this.cancel();"
-                + "        handled = true;"
-                + "        break;"
-                + "    }"
-                + "    if (handled) {"
-                + "        OpenLayers.Event.stop(evt);"
-                + "    }"
-                + "}"
-                + "});"
-        );
-	}
+    public DrawFeatureControl(VectorLayer layer, String handler) {
+        super.create( new Stringer( "new OpenLayers.Control.DrawFeature(", 
+            layer.getJSObjRef(), ", ", handler,
+            // XXX FIX http://polymap.org/atlas/ticket/219
+            // the event: this.events.triggerEvent('featureadded') is not fired for unknown reason
+            // -> do triggerEvent() in timeout which seems to disarm a race cond
+            ",{drawFeature: function(geometry) {",
+                "var self = this; var args = arguments;",
+                "setTimeout( function() {",
+                    "OpenLayers.Control.DrawFeature.prototype.drawFeature.apply(self, args);",
+                "}, 250 );",
+            "},",
+            "}",
+            ");" ).toString() );
+        
+        addObjModCode( new Stringer( "OpenLayers.Util.extend(", getJSObjRef(), ", {",
+            "activate: function() {",
+                "OpenLayers.Control.DrawFeature.prototype.activate.apply(this, arguments);",
+                "if (!this.keyboardHandler) {",
+                    "this.keyboardCallbacks = { keydown: this.handleKeyDown };",
+                    "this.keyboardHandler = new OpenLayers.Handler.Keyboard(this, this.keyboardCallbacks, {});",
+                "}",
+                "this.keyboardHandler.activate();",
+            "},",
+            "deactivate: function() {",
+                "OpenLayers.Control.DrawFeature.prototype.deactivate.apply(this, arguments);",
+                "this.keyboardHandler.deactivate();",
+            "},",
+            "destroy: function() {",
+                "OpenLayers.Control.ModifyFeature.prototype.destroy.apply(this, arguments);",
+                "if (this.keyboardHandler) {",
+                    "this.keyboardHandler.destroy();",
+                "}",
+            "},",
+            "handleKeyDown: function (evt) {",
+                "var handled = false;",
+                "switch (evt.keyCode) {",
+                "case 90:",  // z
+//                        "if (evt.metaKey || evt.ctrlKey) {",
+                    "this.undo();",
+                    "handled = true;",
+//                        "}",
+                    "break;",
+                "case 89:", // y
+//                        "if (evt.metaKey || evt.ctrlKey) {",
+                    "this.redo();",
+                    "handled = true;",
+//                        "}",
+                    "break;",
+                "case 27:", // esc
+                    "this.cancel();",
+                    "handled = true;",
+                    "break;",
+                "}",
+                "if (handled) {",
+                    "OpenLayers.Event.stop(evt);",
+                "}",
+            "}",
+            "});").toString() );
+    }
 
 //    public void addMode( int mode ) {
 //        super.addObjModCode( "obj.mode |=  OpenLayers.Control.ModifyFeature." + mode2name( mode ) );
