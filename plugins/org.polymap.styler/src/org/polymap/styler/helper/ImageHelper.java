d2482fd34ccf7add16a1b90bca8c98227196cce8
falko <falko@localhost>
2013-10-21 11:38:03 +0200

    #14: Vorgang - Schachtschein (https://polymap.org/mosaic/ticket/14)   - getjars.build.xml: missing unless="already_downloaded"

--- plugins/org.polymap.styler/src/org/polymap/styler/helper/ImageHelper.java
+++ plugins/org.polymap.styler/src/org/polymap/styler/helper/ImageHelper.java
@@ -36,7 +36,6 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.graphics.PaletteData;
-import org.eclipse.swt.internal.graphics.ResourceFactory;
 
 
 /**
@@ -52,12 +51,13 @@ public class ImageHelper {
 
 	
     public static Image createColorRectImage( java.awt.Color color ) {
-        return ResourceFactory.findImage( convertToSWT( createColorRectBufferedImage( color ) ) );
+        // FIXME
+        throw new RuntimeException( "FIXME return ResourceFactory.findImage( convertToSWT( createColorRectBufferedImage( color ) )" );
 	}
 	
 
     public static Image createColorRectImage( RGB rgb ) {
-        return ResourceFactory.findImage( convertToSWT( createColorRectBufferedImage( ColorHelper.RGB2Color( rgb ) ) ) );
+        throw new RuntimeException( "FIXME return ResourceFactory.findImage( convertToSWT( createColorRectBufferedImage( ColorHelper.RGB2Color( rgb ) ) )" );
     }
 
 	
