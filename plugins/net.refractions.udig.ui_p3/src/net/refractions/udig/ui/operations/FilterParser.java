16c2da1a87ca642dc10ec05b780ede483b01610c
fb71 <falko@polymap.de>
2015-01-29 19:43:36 +0100

    Saying Good Bye to uDig code. It got me started and helped me a lot. On the other side it wasn't always fun to work with. So, farewell!

--- plugins/net.refractions.udig.ui_p3/src/net/refractions/udig/ui/operations/FilterParser.java
+++
@@ -1,9 +0,0 @@
-package net.refractions.udig.ui.operations;
-
-import org.eclipse.core.runtime.IConfigurationElement;
-
-public interface FilterParser {
-
-    public String getElementName();
-    public OpFilter parse(IConfigurationElement element);
-}
