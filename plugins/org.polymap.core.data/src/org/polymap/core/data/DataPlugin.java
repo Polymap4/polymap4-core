83529e4f46688bb5003d1825783f3a1db0a6db90
Falko Bräutigam <falko@polymap.de>
2016-09-12 22:08:01 +0200

    #85: [Data] Pipeline: native getBounds() (http://github.com/Polymap4/polymap4-core/issues/issue/85)
      - enable Marlin by default

--- plugins/org.polymap.core.data/src/org/polymap/core/data/DataPlugin.java
+++ plugins/org.polymap.core.data/src/org/polymap/core/data/DataPlugin.java
@@ -14,25 +14,29 @@
  */
 package org.polymap.core.data;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-
-import org.polymap.core.ui.ImageRegistryHelper;
-
 import org.geotools.factory.CommonFactoryFinder;
 import org.opengis.filter.FilterFactory2;
 import org.osgi.framework.BundleContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import org.eclipse.swt.graphics.Image;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+import org.polymap.core.ui.ImageRegistryHelper;
 
 /**
  * 
  *
  * @author <a href="http://www.polymap.de">Falko Bräutigam</a>
- * @since 3.0
  */
 public class DataPlugin
         extends AbstractUIPlugin {
 
+    private static final Log log = LogFactory.getLog( DataPlugin.class );
+    
     public static final String PLUGIN_ID = "org.polymap.core.data";
 
     /**
@@ -66,18 +70,9 @@ public class DataPlugin
         super.start( context );
         plugin = this;
         
-//        LayerFeatureSelectionManager.setFeatureCollectionFactory(
-//                new FeatureCollectionFactory() {
-//                    public FeatureCollection newFeatureCollection( Object layer, Filter filter ) {
-//                        try {
-//                            PipelineFeatureSource fs = PipelineFeatureSource.forLayer( (ILayer)layer, false );
-//                            return fs.getFeatures( filter );
-//                        }
-//                        catch (Exception e) {
-//                            throw new RuntimeException( e );
-//                        }
-//                    }
-//                });
+        // does not seem to break things if not available
+        log.info( "Using Marlin render engine if avalable." );
+        System.setProperty( "sun.java2d.renderer", "org.marlin.pisces.PiscesRenderingEngine" );
     }
 
 
